c1bba6f85b280c671df9bfd6abc0c7331bb104a2
Jolie Wang <107016627+joliejywang@users.noreply.github.com>
2025-05-03 15:52:42 -0700

    adc references

---
+++ adc12_sequence_conversion/Debug/adc12_sequence_conversion.d
@@ -0,0 +1,225 @@
+# FIXED
+
+adc12_sequence_conversion.o: ../adc12_sequence_conversion.c \
+ ti_msp_dl_config.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/msp.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/DeviceFamily.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/mspm0g350x.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/third_party/CMSIS/Core/Include/core_cm0plus.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_adc12.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_aes.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_comp.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_crc.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_dac12.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_dma.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_flashctl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_gpio.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_gptimer.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_i2c.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_iomux.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_mathacl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_mcan.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_oa.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_rtc.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_spi.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_trng.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_uart.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_vref.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_wuc.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_wwdt.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_factoryregion.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_cpuss.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_debugss.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_sysctl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/sysctl/hw_sysctl_mspm0g1x0x_g3x0x.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/driverlib.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_adc12.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_common.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_factoryregion.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_core.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_aes.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_aesadv.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_comp.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_crc.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_crcp.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_dac12.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_dma.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_flashctl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_sysctl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/sysctl/dl_sysctl_mspm0g1x0x_g3x0x.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_gpamp.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_gpio.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_i2c.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_iwdt.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_lfss.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_keystorectl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_lcd.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_mathacl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_mcan.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_opa.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_rtc.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_rtc_common.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_rtc_a.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_rtc_b.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_scratchpad.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_spi.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_tamperio.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_timera.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_timer.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_timerg.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_trng.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_uart_extend.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_uart.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_uart_main.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_vref.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_wwdt.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_interrupt.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_systick.h
+
+ti_msp_dl_config.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/msp.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/DeviceFamily.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/mspm0g350x.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/third_party/CMSIS/Core/Include/core_cm0plus.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_adc12.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_aes.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_comp.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_crc.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_dac12.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_dma.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_flashctl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_gpio.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_gptimer.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_i2c.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_iomux.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_mathacl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_mcan.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_oa.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_rtc.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_spi.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_trng.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_uart.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_vref.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_wuc.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_wwdt.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_factoryregion.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_cpuss.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_debugss.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_sysctl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/sysctl/hw_sysctl_mspm0g1x0x_g3x0x.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/driverlib.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_adc12.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_common.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_factoryregion.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_core.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_aes.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_aesadv.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_comp.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_crc.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_crcp.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_dac12.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_dma.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_flashctl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_sysctl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/sysctl/dl_sysctl_mspm0g1x0x_g3x0x.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_gpamp.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_gpio.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_i2c.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_iwdt.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_lfss.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_keystorectl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_lcd.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_mathacl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_mcan.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_opa.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_rtc.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_rtc_common.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_rtc_a.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_rtc_b.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_scratchpad.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_spi.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_tamperio.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_timera.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_timer.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_timerg.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_trng.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_uart_extend.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_uart.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_uart_main.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_vref.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/dl_wwdt.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_interrupt.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/driverlib/m0p/dl_systick.h:
---
+++ adc12_sequence_conversion/Debug/startup_mspm0g350x_ticlang.d
@@ -0,0 +1,94 @@
+# FIXED
+
+startup_mspm0g350x_ticlang.o: \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/msp.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/DeviceFamily.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/mspm0g350x.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/third_party/CMSIS/Core/Include/core_cm0plus.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_adc12.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_aes.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_comp.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_crc.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_dac12.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_dma.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_flashctl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_gpio.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_gptimer.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_i2c.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_iomux.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_mathacl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_mcan.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_oa.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_rtc.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_spi.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_trng.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_uart.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_vref.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_wuc.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_wwdt.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_factoryregion.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_cpuss.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_debugss.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_sysctl.h \
+ /Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/sysctl/hw_sysctl_mspm0g1x0x_g3x0x.h
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/msp.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/DeviceFamily.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/mspm0g350x.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/third_party/CMSIS/Core/Include/core_cm0plus.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_adc12.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_aes.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_comp.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_crc.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_dac12.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_dma.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_flashctl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_gpio.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_gptimer.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_i2c.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_iomux.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_mathacl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_mcan.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_oa.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_rtc.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_spi.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_trng.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_uart.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_vref.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_wuc.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/hw_wwdt.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_factoryregion.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_cpuss.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_debugss.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/hw_sysctl.h:
+
+/Users/joliewang/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/peripherals/m0p/sysctl/hw_sysctl_mspm0g1x0x_g3x0x.h:
